47b0bdf0fd90c1add01e92e6a1079d5ccde52499
Valmir Dornas <fernandocursos2021@gmail.com>
2025-08-02 17:35:22 -0300

    Alterando magens para a pasta public

--- app/Services/ImageService.php
+++ app/Services/ImageService.php
@@ -2,13 +2,15 @@
 
 namespace App\Services;
 
+use Error;
+
 class ImageService
 {
     public static function storeImages(
         array|object $images,
         string $pathToStore,
         string|int $propertyKey = 'propertyKey',
-        string|int|null $propertyValue = null
+        string|int $propertyValue = ''
     ): array {
 
         // É apenas uma imagem (objeto de uma imagem 'upada')?
@@ -18,7 +20,6 @@ class ImageService
         }
 
         // Temos uma array de imagens
-
         $uploadedImages = [];
 
         foreach ($images['images'] as $image) {
@@ -36,11 +37,9 @@ class ImageService
     public static function showImage(string $imagePath, string $image, string $sizeImage = 'regular')
     {
         if ($sizeImage == 'small') {
-
-            $imagePath = WRITEPATH . "uploads/$imagePath/small/$image";
+            $imagePath = ROOTPATH . "public/assets/images/$imagePath/small/$image";
         } else {
-
-            $imagePath = WRITEPATH . "uploads/$imagePath/$image";
+            $imagePath = ROOTPATH . "public/assets/images/$imagePath/$image";
         }
 
         $fileInfo = new \finfo(FILEINFO_MIME);
@@ -56,17 +55,12 @@ class ImageService
         exit;
     }
 
-    /**
-     * Método responsável em excluir imagens no file sytem
-     *
-     * @param string $pathToImage
-     * @param string $imageToDestroy
-     * @return void
-     */
+
     public static function destroyImage(string $pathToImage, string $imageToDestroy)
     {
-        $regularImageToDestroy  = WRITEPATH . "uploads/{$pathToImage}/{$imageToDestroy}";
-        $smallImageToDestroy    = WRITEPATH . "uploads/{$pathToImage}/small/{$imageToDestroy}";
+        $regularImageToDestroy  = ROOTPATH . "public/assets/images/{$pathToImage}/{$imageToDestroy}";
+
+        $smallImageToDestroy    = ROOTPATH . "public/assets/images/{$pathToImage}/small/{$imageToDestroy}";
 
         if (is_file($regularImageToDestroy)) {
 
@@ -83,31 +77,26 @@ class ImageService
     private static function worksWithImage(object $image, string $pathToStore): string
     {
 
-        // Nesse ponto armazenamos a imagem no caminho informado.
-        $imagePath = $image->store($pathToStore);
+        $image->move(ROOTPATH . "public/assets/images/$pathToStore", $image->getRandomName());
 
-        // Fullpath de onde foi armazenado o arquivo
-        $imagePath = WRITEPATH . "uploads/$imagePath";
+        $imagePath = "{$pathToStore}/{$image->getName()}";
 
+        $imagePath = ROOTPATH . "public/assets/images/{$imagePath}";
 
-        $imageSmallPath = WRITEPATH . "uploads/$pathToStore/small/";
+        $imageSmallPath =  ROOTPATH . "public/assets/images/$pathToStore/small/";
 
 
         // Existe o diretório contido na variável $imageSmallPath?
         if (!is_dir($imageSmallPath)) {
-
             // Não existe. Então podemos criá-lo
             mkdir($imageSmallPath);
         }
 
-
         // Manulamos a imagem para criamos uma cópia um pouco menor que a original
-
         service('image')
             ->withFile($imagePath) // arquivo original
             ->resize(275, 275, true, 'center')
             ->save($imageSmallPath . $image->getName());
-
 
         return $image->getName();
     }
