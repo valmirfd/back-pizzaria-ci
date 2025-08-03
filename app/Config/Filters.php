c30f3d2dc354696c9337e06f3f0ed08a56003a63
Valmir Dornas <fernandocursos2021@gmail.com>
2025-08-03 13:23:42 -0300

    Cors filter

--- app/Config/Filters.php
+++ app/Config/Filters.php
@@ -2,6 +2,7 @@
 
 namespace Config;
 
+use App\Filters\CorsFilter;
 use CodeIgniter\Config\Filters as BaseFilters;
 use CodeIgniter\Filters\Cors;
 use CodeIgniter\Filters\CSRF;
@@ -34,6 +35,7 @@ class Filters extends BaseFilters
         'forcehttps'    => ForceHTTPS::class,
         'pagecache'     => PageCache::class,
         'performance'   => PerformanceMetrics::class,
+        'api-cors'      => CorsFilter::class,
     ];
 
     /**
@@ -103,5 +105,10 @@ class Filters extends BaseFilters
      *
      * @var array<string, array<string, list<string>>>
      */
-    public array $filters = [];
+    public array $filters = [
+        'api-cors' => [
+            'before' => ['api/*'],
+            'after'  => ['api/*'],
+        ]
+    ];
 }
